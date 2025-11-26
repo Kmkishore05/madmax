27d6891535e4d2cd463c82fe0ba746ec4ea5abfe
KISHORE K M <119513011+Kmkishore05@users.noreply.github.com>
2025-11-26 12:45:24 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,22 +3,32 @@ import "./Home.css";
 import { useNavigate } from "react-router-dom";
 import logoImage from "../assets/rb-logo.png";
 import hoverSound from "../assets/audio.mp3";
+import bg from "../assets/home.jpg";
 
 const Home = () => {
   const navigate = useNavigate();
 
   const goToDome = () => {
-    navigate("/domegallery");  // <-- correct URL path
+    navigate("/domegallery");
   };
-    const playHoverSound = () => {
+
+  const playHoverSound = () => {
     const audio = new Audio(hoverSound);
-    audio.volume = 0.4;    // adjust loudness
+    audio.volume = 0.4;
     audio.play();
   };
 
   return (
-    <div className="home-container">
-
+    <div
+      className="home-container"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+      }}
+    >
       {/* Logo */}
       <header className="home-header">
         <img src={logoImage} alt="Logo" className="home-logo" />
@@ -28,19 +38,17 @@ const Home = () => {
       <section className="hero-section">
         <h1 className="hero-title">MAX</h1>
 
-        {/* Button on top of car */}
-       <button
+        {/* Button */}
+        <button
           className="hero-btn"
           onClick={goToDome}
-          onMouseEnter={playHoverSound}     // <-- Sound on hover
+          onMouseEnter={playHoverSound}
         >
           View Dome
         </button>
-
-        {/* Car Image */}
-      
       </section>
     </div>
   );
 };
+
 export default Home;
